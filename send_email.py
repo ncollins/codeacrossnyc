599fa5ec8570eda0cc5945c2fb01fc54238f46ea
Nick Collins <nick@vividx.co.uk>
2014-02-23 16:00:26 -0500

    fleshes out email

--- send_email.py
+++ send_email.py
@@ -8,10 +8,12 @@ from email.mime.multipart import MIMEMultipart
 
 import jinja2
 
+import api
 
-def create_email():
+
+def create_email(json_data):
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = 'test email'
+    msg['Subject'] = 'Community Board 09 Manhattan landlord action report'
     msg['From'] = 'test@test.com'
     msg['To'] = 'collins.nicholas@gmail.com'
     msg.preamble = 'the is the preamble'
@@ -20,10 +22,10 @@ def create_email():
     html = '<h1>Hello!</h1><p>This is the HTML version of the email</>'
 
     part1 = MIMEText(text, 'plain')
-    #part2 = MIMEText(html, 'html')
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('/Users/nick/code/hackathons/codeacrossnyc'))
     template = env.get_template('email.html') 
-    part2 = MIMEText(template.render({'image_name': 'cid:image1'}), 'html')
+    part2 = MIMEText(template.render(json_data), 'html')
+    #part2 = MIMEText(template.render({'image_name': 'cid:image1'}), 'html')
 
     msg.attach(part1)
     msg.attach(part2) # part2 is the prefered type
@@ -49,12 +51,13 @@ if __name__ == '__main__':
     with open(args.emails) as f:
         emails = f.read().split()
 
-    if emails:
-        email, password = get_email_and_password()
-    
-        msg = create_email()
+    email, password = get_email_and_password()
 
-        s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        s.login(email, password)
-        s.sendmail(email, ', '.join(emails), msg.as_string())
-        s.quit()
+    json_data = api.final_data()
+    
+    msg = create_email(json_data)
+
+    s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    s.login(email, password)
+    s.sendmail(email, ', '.join(emails), msg.as_string())
+    s.quit()
